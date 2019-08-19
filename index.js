93f16a2a9781004da173e16aad84db947b51ff30
Erica Murdock-Waters <ericahpa@gmail.com>
2019-08-19 05:33:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'Welcome, ' + alert(currentUser[0]) + '!';
+const shortGreeting = 'Welcome, ' + chart(currentUser[0]) + '!';
