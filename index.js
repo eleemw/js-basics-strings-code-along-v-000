16c509c05ba60ddd5b536198981bc2f361d8a9c6
Erica Murdock-Waters <ericahpa@gmail.com>
2019-08-19 05:51:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 //const shortGreeting = 'Welcome, ' + (currentUser.substring(0,1)) + '!';
 //method above also passed the tests
 //const shortGreeting = 'Welcome, ' + (currentUser.charAt(0)) + '!';
-const shortGreeting = 'Welcome, ' + '${currentUser}[0]' + '!';
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
