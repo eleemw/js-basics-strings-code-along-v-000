a95e42a96a28794ef7c4b6d974a0ee6bc56e759b
Erica Murdock-Waters <ericahpa@gmail.com>
2019-08-19 05:47:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 //const shortGreeting = 'Welcome, ' + (currentUser.substring(0,1)) + '!';
+//method above also passed the tests 
 const shortGreeting = 'Welcome, ' + (currentUser.charAt(0)) + '!';
